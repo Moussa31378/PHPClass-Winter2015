863c3ea7932e5ff7c08fc2effe8ba3daf6eb2ddb
Moussa <Moussa@10.240.210.71>
2015-02-04 19:22:02 -0500

    week2 Demo2

--- Week2/Lab/Ex/ch02_ex1/display_discount.php
+++ Week2/Lab/Ex/ch02_ex1/display_discount.php
@@ -6,6 +6,17 @@
     <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 <body>
+    <?php
+        $product_description = $_POST['product_description'];
+        $list_price = $_POST['list_price'];
+        $discount_percent = $_POST['discount_percent'];
+        $discount = $list_price * $discount_percent * .01;
+        $discount_price = $Slist_price - $discount;
+        $list_price_formatted = "$".number_format($list_price, 2);
+        $discount_percent_formatted = $discount_percent."%";
+        $discount_formatted = "$".number_format($discount, 2);
+        $discount_price_formatted = "$".number_format($discount_price, 2);
+    ?>
     <div id="content">
         <h1>This page is under construction</h1>
 
